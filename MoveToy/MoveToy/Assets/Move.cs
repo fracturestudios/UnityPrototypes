266e063763c4a0947b4b70214fec66257281b453
Dave Kilian <dave.kilian@gmail.com>
2015-08-15 11:51:28 -0700

    Collision detection response

--- MoveToy/MoveToy/Assets/Move.cs
+++ MoveToy/MoveToy/Assets/Move.cs
@@ -16,21 +16,39 @@ public class Move : MonoBehaviour
 		Vector3 forward = transform.rotation * new Vector3(0, 0, 1);
 		Vector3 right = transform.rotation * new Vector3(1, 0, 0);
 
+		Vector3 velocity = Vector3.zero;
+
 		if (Input.GetButton(BTN_FORWARD)) {
-			transform.position += Speed * Time.deltaTime * forward;
+			velocity += Speed * forward;
 		}
 
 		if (Input.GetButton(BTN_BACK)) {
-			transform.position -= Speed * Time.deltaTime * forward;
+			velocity -= Speed * forward;
 		}
 
 		if (Input.GetButton(BTN_LEFT)) {
-			transform.position -= Speed * Time.deltaTime * right;
+			velocity -= Speed * right;
 		}
 
 		if (Input.GetButton(BTN_RIGHT)) {
-			transform.position += Speed * Time.deltaTime * right;
+			velocity += Speed * right;
 		}
+
+		GetComponent<Rigidbody>().velocity = velocity;
+	}
+
+	void OnCollisionEnter(Collision c)
+	{
+		Transform t = GetComponent<Transform>();
+		Rigidbody rb = GetComponent<Rigidbody>();
+
+		Vector3 v = rb.velocity;
+		foreach (ContactPoint pt in c.contacts) {
+			Vector3 normal = Vector3.Normalize(pt.point - t.position);
+			v = Vector3.Reflect(v, normal);
+		}
+
+		rb.velocity = v;
 	}
 
 	private static string BTN_FORWARD = "MoveForward";
