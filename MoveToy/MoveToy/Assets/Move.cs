0bc87ec381fce9e057afb3324b79ddb615d2f096
Dave Kilian <dave.kilian@gmail.com>
2015-08-15 17:49:00 -0700

    Base movement on force/drag

--- MoveToy/MoveToy/Assets/Move.cs
+++ MoveToy/MoveToy/Assets/Move.cs
@@ -5,9 +5,7 @@ public class Move : MonoBehaviour
 {
 	public float Speed = 4.0f;
 
-	void Start()
-	{
-	}
+	void Start() { }
 
 	void Update()
 	{
@@ -16,39 +14,25 @@ public class Move : MonoBehaviour
 		Vector3 forward = transform.rotation * new Vector3(0, 0, 1);
 		Vector3 right = transform.rotation * new Vector3(1, 0, 0);
 
-		Vector3 velocity = Vector3.zero;
+		Vector3 force = Vector3.zero;
 
 		if (Input.GetButton(BTN_FORWARD)) {
-			velocity += Speed * forward;
+			force += Speed * forward;
 		}
 
 		if (Input.GetButton(BTN_BACK)) {
-			velocity -= Speed * forward;
+			force -= Speed * forward;
 		}
 
 		if (Input.GetButton(BTN_LEFT)) {
-			velocity -= Speed * right;
+			force -= Speed * right;
 		}
 
 		if (Input.GetButton(BTN_RIGHT)) {
-			velocity += Speed * right;
+			force += Speed * right;
 		}
 
-		GetComponent<Rigidbody>().velocity = velocity;
-	}
-
-	void OnCollisionEnter(Collision c)
-	{
-		Transform t = GetComponent<Transform>();
-		Rigidbody rb = GetComponent<Rigidbody>();
-
-		Vector3 v = rb.velocity;
-		foreach (ContactPoint pt in c.contacts) {
-			Vector3 normal = Vector3.Normalize(pt.point - t.position);
-			v = Vector3.Reflect(v, normal);
-		}
-
-		rb.velocity = v;
+		GetComponent<Rigidbody>().AddForce(force);
 	}
 
 	private static string BTN_FORWARD = "MoveForward";
